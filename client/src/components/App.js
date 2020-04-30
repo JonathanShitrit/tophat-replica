10c1eed21a0fdf18974e0cdf4c1d3cacf5167af3
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2020-04-30 15:20:49 -0400

    prepare code for heroku deployment

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+
+
+  // use fetch(`${document.location.origin}/)
+
+
   render() {
     return (
       <div>React App</div>

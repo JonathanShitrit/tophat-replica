d2c85f0e7428dfbba7b87aa23458e8c4b94a17f1
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2020-04-30 14:14:33 -0400

    combined front with back

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const request = require("request");
+const path = require("path");
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("request details. Method", req.method, "original url:", req.originalUrl);
+
+    next();
+});
+
+// Handles the CORS error
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    next();
+});
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("/ping", (req, res) => {
+    res.send("pong!");
+});
+
+
+
+// handle errors middleware
+app.use((err, req, res, next) => {
+    console.log("err", err);
+
+    res.status(500).json({ type: "error", message: err.message });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));

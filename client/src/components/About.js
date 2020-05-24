b638eda7932a1bd5923e4fb6523ae8a4e500ee0a
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2020-05-23 22:37:12 -0400

    Help and About

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,22 +2,28 @@ import React, { Component } from "react";
 
 class About extends Component {
     render() {
+        const leftAlign = {
+            textAlign: "left"
+        }
+
         return (
-            <div className="">
-                <p>Karishma Muni </p>
+            <div style={{ padding: "20px" }}>
+                <p>Indelible Interact is a platform that allows instructors to create assignments ans questions that tests a students understanding of the material.</p>
+                <br />
+                <h2 style={leftAlign}>Karishma Muni </h2>
                 <p>
-                Hi There! My name is Karishma and I am a graduating senior at Queens College CUNY majoring in Computer Science. I recently got interested in web development and am enjoying web applications development. Thank you so much for visiting my website!
+                    Hi There! My name is Karishma and I am a graduating senior at Queens College CUNY majoring in Computer Science. I recently got interested in web development and am enjoying web applications development. Thank you so much for visiting my website!
                 </p>
 
 
-                <p>Jonathan Shitrit</p>
+                <h2 style={leftAlign}>Jonathan Shitrit</h2>
                 <p>
-
+                    Hi I'm Jonathan, an undergraduate Computer Science student at CUNY Queens College. I have built a few iOS apps with Swift and I am currently learning the Full Stack web development with React and Node.js.
                 </p>
 
-                <p>Zachary Wing</p>
+                <h2 style={leftAlign}>Zachary Wing</h2>
                 <p>
-                I am an undergraduate student at Queens College, City University of New York studying computer science and mathematics.
+                    I am an undergraduate student at Queens College, City University of New York studying computer science and mathematics.
                 </p>
             </div>
         )
--- client/src/components/Help.js
+++ client/src/components/Help.js
@@ -2,9 +2,19 @@ import React, { Component } from "react";
 
 class Help extends Component {
     render() {
+        const leftAlign = {
+            textAlign: "left"
+        }
         return (
-            <div className="">
-                Help
+            <div style={{ padding: "20px" }}>
+                <h3 style={leftAlign}>Get Started for Instructors</h3>
+                <p>First create questions that can be multiple choice or textbox short answer.</p>
+                <p>Next creat a Question Set and add all the relevant questions to that set.</p>
+                <br />
+                <h3 style={leftAlign}>Get Started for Students</h3>
+                <p>"Classwork" will show all the question sets that pertain to you.</p>
+                <p>Complete a question set in Classwork and then click on "Gradebook".</p>
+                <p>"Gradebook" shows your score to that corresponding question set.</p>
             </div>
         )
     }
